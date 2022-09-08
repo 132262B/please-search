514634b0f3e4f93a9bfa6c19dab384e8aafe8520
Igor <132262b@naver.com>
2022-09-08 20:43:22 +0900

    [⌨️dev] npm lib(vue writer) apply.

--- src/components/main/MainTop.vue
+++ src/components/main/MainTop.vue
@@ -13,7 +13,9 @@
           <div class="flex-shrink max-w-full px-4 w-full md:w-9/12 lg:w-1/2 self-center lg:pr-12">
             <div class="text-center lg:text-left mt-6 lg:mt-0">
               <div class="mb-12">
-                <h1 class="text-4xl leading-normal text-black font-bold mb-4">Please search your question on the Internet.</h1>
+                <h1 class="text-4xl leading-normal text-black font-bold mb-4">
+                  <VueWriter :array="arr" :typeSpeed="70" :caret="cursor" />
+                </h1>
                 <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">If you search for it, you can see everything</p>
               </div>
               <a class="py-2.5 px-10 inline-block text-center leading-normal text-gray-900 bg-white border-b border-gray-100 hover:text-black hover:ring-0 focus:outline-none focus:ring-0 mr-4" href="#home">
@@ -38,10 +40,47 @@ export default {
   data() {
     return {
       homeSearchImage: require('@/assets/images/search.svg'),
+      arr: ['Please search your question on the Internet.', '검색하면 다나와요.'],
     };
   },
 };
 </script>
 
 <style>
+.is-typed span.typed {
+  color: black;
+}
+
+.is-typed span.cursor {
+  display: inline-block;
+  width: 2px;
+  height: 50px;
+  background-color: black;
+  animation: blink 1s infinite;
+}
+
+.is-typed span.underscore {
+  display: inline-flex;
+  width: 10px;
+  height: 1px;
+  align-items: flex-end;
+  background-color: black;
+  animation: blink 1s infinite;
+}
+
+.is-typed span.cursor.typing {
+  animation: none;
+}
+
+@keyframes blink {
+  49% {
+    background-color: black;
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import router from './router'
 import './style/common.css'
 import axios from 'axios'
 
+import VueWriter from "vue-writer";
+
 //axios.defaults.baseURL = 'http://localhost:8080'
 
 const app = createApp(App)
 app.config.globalProperties.axios = axios;
 
 app.use(router)
+    .use(VueWriter)
     .mount('#app')
